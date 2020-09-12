0107e9abdb547d0835e4bfe0f04d3d37af75fea9
schetininl <salexbin@bk.ru>
2020-09-12 15:33:57 +0300

    index page get params

---
+++ recipes/templatetags/set_get_params.py
@@ -0,0 +1,29 @@
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag
+def set_tags(request, tags, value):
+    dict_ = request.GET.copy()
+    #Если есть такой параметр в Get-запросе, то удаляем его
+    if request.GET.get(value):
+        del dict_[value]
+    #Если такого параметра нет, но есть в переменной tags
+    elif value in tags:
+        for tag in tags:
+            #Устанавливаем остальные теги(кроме выбранного)
+            if tag != value:
+                dict_[tag] = "tag"
+    #Иначе мы добавляем этот параметр
+    else:
+        dict_[value] = "tag"
+
+    return dict_.urlencode()
+
+
+@register.simple_tag
+def set_page(request, value):
+    dict_ = request.GET.copy()
+    dict_["page"] = value
+    return dict_.urlencode()
--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.paginator import Paginator
+import operator
+from django.db.models import Q
+from functools import reduce
 
-from .models import Recipe
+from .models import TAG_CHOICES, Recipe
 
 
 def index(request):
-    latest = Recipe.objects.order_by("-pk")[:6]
-    return render(request, "index.html", {"recipes": latest})
+    tags = []
+    for label, _ in TAG_CHOICES:
+        if request.GET.get(label, ""):
+            tags.append(label)
+    if tags:
+        tagsFilter = reduce(operator.or_, (Q(tags__contains=tag)
+                                           for tag in tags))
+        recipes = Recipe.objects.filter(tagsFilter).order_by("-pk")
+    else:
+        tags = [label for label, _ in TAG_CHOICES]
+        recipes = Recipe.objects.order_by("-pk")
+    paginator = Paginator(recipes, 6)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+    return render(
+        request,
+        'index.html',
+        {'page': page, 'paginator': paginator, 'tags': tags}
+    )
