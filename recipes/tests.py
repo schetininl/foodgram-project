fbb7f9a3f1d147b8eafc72bf3ac9cf2d775957d8
schetininl <salexbin@bk.ru>
2020-09-17 10:10:49 +0300

    test index page added

---
+++ recipes/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase, Client
+# Create your tests here.
+
+class IndexPageTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+    
+    def test_user_post(self):
+        """Доступна ли главная страница"""
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
