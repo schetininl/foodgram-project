f362686084846be24b35286c2bace62bdeeac48f
schetininl <salexbin@bk.ru>
2020-09-19 15:05:45 +0300

    misc pages

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -26,6 +26,9 @@ urlpatterns = [
     path("", include("recipes.urls")),
 ]
 
+handler404 = "recipes.views.page_not_found"
+handler500 = "recipes.views.server_error"
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
--- recipes/views.py
+++ recipes/views.py
@@ -182,3 +182,11 @@ def wishlist(request):
         'recipes': recipes
     }
     return render(request, 'wishlist.html', context)
+
+
+def page_not_found(request, exception):
+    return render(request, "misc/404.html", {"path": request.path}, status=404)
+
+
+def server_error(request):
+    return render(request, "misc/500.html", status=500)
